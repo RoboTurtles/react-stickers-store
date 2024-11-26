a9850a0dcacae347596cd29b57e5c45cbdfb40d5
Calvin Liu <liucalvin12@gmail.com>
2024-11-26 18:33:54 +0000

    Set up TailwindCSS with React

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white text-center p-4">
+        Welcome to React with Tailwind CSS!
+      </header>
+    </div>
+  );
+}
+
+export default App;
